0b8c7afd303d0281a9280dc748a7a3ff5958c533
Gabriel Silguero <ikdhd@plaiaundi.net>
2024-11-14 09:51:33 +0100

    otroCommitBranch2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esta es la 2da Branch actualizada");
+        System.out.println("Esta es la 2da Branch actualizada otra vez");
     }
 }
