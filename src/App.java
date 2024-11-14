bd7264f333cad0735056e13ea801120d206760af
Gabriel Silguero <ikdhd@plaiaundi.net>
2024-11-14 09:50:46 +0100

    commitBranch1antesMerge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Esta es la verdadera prueba para el examen");
+        System.out.println("Esta es la Branch principal");
     }
 }
